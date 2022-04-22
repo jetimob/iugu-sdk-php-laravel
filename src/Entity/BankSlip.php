4421405eb8b28b2783a7c9035f2aedc9862b6d82
medina <ammedina@inf.ufsm.br>
2022-04-22 16:02:49 -0300

    fix: adiciona getters para BankSlip

--- src/Entity/BankSlip.php
+++ src/Entity/BankSlip.php
@@ -7,4 +7,19 @@ class BankSlip extends Entity
     protected string $digitable_line;
     protected string $barcode_data;
     protected string $barcode;
+
+    public function getDigitableLine(): string
+    {
+        return $this->digitable_line;
+    }
+
+    public function getBarcodeData(): string
+    {
+        return $this->barcode_data;
+    }
+
+    public function getBarcode(): string
+    {
+        return $this->barcode;
+    }
 }
