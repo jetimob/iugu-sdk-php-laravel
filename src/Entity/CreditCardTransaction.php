3f5a3088ca192c5bdf7754fe1950c0a1dd1d5af0
doc <alexcsouza@outlook.com>
2023-05-03 15:55:07 -0300

    fix: permite null nos atributos da antidade CreditCardTransaction

--- src/Entity/CreditCardTransaction.php
+++ src/Entity/CreditCardTransaction.php
@@ -4,18 +4,18 @@ namespace Jetimob\Iugu\Entity;
 
 class CreditCardTransaction extends Entity
 {
-    protected string $status;
-    protected string $message;
-    protected bool $success;
-    protected string $lr;
-    protected bool $reversible;
-    protected string $token;
-    protected string $brand;
-    protected string $bin;
-    protected string $last4;
-    protected string $created_at;
-    protected string $updated_at;
-    protected string $authorized_at;
-    protected string $captured_at;
-    protected string $canceled_at;
+    protected ?string $status = null;
+    protected ?string $message = null;
+    protected ?bool $success = null;
+    protected ?string $lr = null;
+    protected ?bool $reversible = null;
+    protected ?string $token = null;
+    protected ?string $brand = null;
+    protected ?string $bin = null;
+    protected ?string $last4 = null;
+    protected ?string $created_at = null;
+    protected ?string $updated_at = null;
+    protected ?string $authorized_at = null;
+    protected ?string $captured_at = null;
+    protected ?string $canceled_at = null;
 }
